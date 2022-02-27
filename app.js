7b37e4df730ab31c75217c4d722c40d412e81ab5
shubham <shubhampwankhede9921@gmail.com>
2022-02-27 16:38:12 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,76 @@
+require('dotenv').config()
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+const encrypt=require("mongoose-encryption")
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+const userSchema=new mongoose.Schema({
+    username:String,
+    password:String
+});
+
+userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields:["password"]});
+const User=mongoose.model("User",userSchema);
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+//TODO
+
+app.get("/",function(req,res){
+    res.render("home")
+});
+
+app.get("/login",function(req,res){
+    res.render("login")
+});
+
+app.get("/register",function(req,res){
+    res.render("register")
+});
+
+app.get("/submit",function(req,res){
+    res.render("submit")
+});
+
+app.post("/register",function(req,res){
+    const newUser=new User({
+        username:req.body.username,
+        password:req.body.password
+    })
+    newUser.save(function(err){
+        if(!err)
+        {
+            res.render("secrets");
+        }
+    });
+
+})
+
+app.post("/login",function(req,res){
+    
+    
+    User.findOne({username:req.body.username},function(err,foundUser){
+        if(!err)
+        {
+            if(foundUser)
+            {
+                if(foundUser.password===req.body.password)
+                {
+                    res.render("secrets");
+                }
+                else{
+                    res.send(err);
+                }
+            }
+        }
+    })
+})
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
